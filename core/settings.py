01f5c95694e1690f1d3254542bf82ca46339b40d
Kazion500 <patrickckabwe@gmail.com>
2021-06-05 14:26:14 +0200

    add domain to allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = evn("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sam-ngo.herokuapp.com","localhost","127.0.0.1"]
 
 
 # Application definition
